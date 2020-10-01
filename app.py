7b87ac114c8b22d0e98d69a536f638950aae1448
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-01 11:38:14 +0000

    Corrected parentheses error in lines 13 and 15 of app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP')),
+    app.run(host=os.environ.get('IP'),
     port = int(os.environ.get('PORT')),
-    debug = True
+    debug = True)
