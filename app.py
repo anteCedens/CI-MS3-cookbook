206cc18f189e199a2ce2fdadcc580be5bf6a43f1
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-04 21:11:51 +0000

    Installed flask-pymongo and dnspython. Updated requirements.txt. Added imports of path, PyMongo, ObjectId, render_template, redirect, request, and url_for to app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask
+from os import path
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
