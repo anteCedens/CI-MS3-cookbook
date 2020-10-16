fb1028ab35ff3ccdb3b77511f1a31c9181567235
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-16 05:04:45 +0000

    Adding comments to script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,17 @@
+// Various jQuery initialization code for corresponding Materialize components and elements
+
 $(document).ready(function () {
   $(".carousel").carousel();
+
+// Slightly increases (2500) the interval of moving to the next slide for the carousel
   setInterval(function () {
     $(".carousel").carousel("next");
   }, 2500);
+
+// Materialize's site (https://materializecss.com/select.html) actually lists a different jQuery code here
+// but that did not work for me - the 'select' element wouldn't initialize
+// so I refered here for the code that worked: 
+                // https://github.com/Code-Institute-Solutions/TaskManager/tree/master/01-PuttingTheBasicsInPlace/07-accordion_setup
   $("select").material_select();
   $(".collapsible").collapsible();
   $(".button-collapse").sideNav();
